f0f3bd77220c7aafd651d8a180519f7f87f735b7
Dawid Czyżewski <dawidek12.04@wp.pl>
2020-12-25 00:51:42 +0100

    Update
    Signed-off-by: Dawid Czyżewski <dawidek12.04@wp.pl>

--- app/src/main/java/com/example/mobileclickergame/LevelsActivity.java
+++ app/src/main/java/com/example/mobileclickergame/LevelsActivity.java
@@ -12,6 +12,8 @@ import java.util.List;
 public class LevelsActivity extends AppCompatActivity {
     DataBaseHelper dataBaseHelper = new DataBaseHelper(LevelsActivity.this);
     int page=1;
+    int [] levels1= new int[101];
+    int [] exps1=new int[101];
     TextView lvl1,lvl2,lvl3,lvl4,lvl5;
     TextView exp1,exp2,exp3,exp4,exp5;
     TextView page1;
@@ -21,17 +23,20 @@ public class LevelsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
 
-
+        lvl1=findViewById(R.id.textView3);
+        exp1=findViewById(R.id.textView4);
+        uploadLevels();
         showLevels(dataBaseHelper);
     }
 
     public void showLevels(DataBaseHelper dataBaseHelper){
-        List<Level> levels = dataBaseHelper.getAllLevels();
         page1 = findViewById(R.id.textView);
         page1.setText("Page: "+page+"/20");
+        lvl1.setText(""+levels1[1]);
+        exp1.setText(""+exps1[1]);
     }
 
-    public void nextPage(){
+    public void nextPage(View view){
         page++;
         page1.setText("Page: "+page+"/20");
     }
@@ -46,5 +51,17 @@ public class LevelsActivity extends AppCompatActivity {
             page--;
         }
         page1.setText("Page: "+page+"/20");
+    }
+
+    public void uploadLevels(){
+        List<Level> levels = dataBaseHelper.getAllLevels();
+        for(int i=0;i<=100;i++){
+            Level level=levels.get(i);
+            levels1[i]=level.getLevel();
+        }
+        for(int i=0;i<=100;i++){
+            Level level=levels.get(i);
+            exps1[i]=level.getExpRequire();
+        }
     }
 }
